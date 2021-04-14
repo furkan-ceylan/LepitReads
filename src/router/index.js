b1c81dac7da5782dc397aaae2d63855cc464f69a
furkan ceylan <32003938+fcyln@users.noreply.github.com>
2021-04-14 05:49:53 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Searchs from '../views/Searchs.vue'
 
 Vue.use(VueRouter)
 
@@ -10,11 +9,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/Searchs',
-    name: 'Searchs',
-    component: Searchs
   },
 ]
 
