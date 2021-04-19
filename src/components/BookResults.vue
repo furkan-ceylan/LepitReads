834146363e1a1d1e75f1c7e9f395ae0ca51b6f3c
furkan ceylan <32003938+fcyln@users.noreply.github.com>
2021-04-19 23:32:04 +0300

    Update BookResults.vue

--- src/components/BookResults.vue
+++ src/components/BookResults.vue
@@ -1,53 +1,7 @@
 <template>
   <div class="book-results">
     <div v-if="books.length > 0">
-      <!-- <div v-else class="had-container">
-      <div class="carousel">
-        <div
-          class="carousel-item card large animated fadeIn"
-          v-for="book in books"
-          :key="book.id"
-        >
-          <div class="card-image waves-effect waves-block waves-light">
-            <img
-              v-if="book.volumeInfo.imageLinks"
-              :src="book.volumeInfo.imageLinks.thumbnail"
-              class="activator"
-            />
-          </div>
-          <div class="card-content">
-            <span v-if="book.volumeInfo.title" class="card-title truncate">{{
-              book.volumeInfo.title
-            }}</span>
-            <p class="authors" v-if="book.volumeInfo.authors">
-              {{ book.volumeInfo.authors[0] }}
-            </p>
-            <p><a class="activator">See More</a></p>
-          </div>
-          <div class="card-action">
-            <span class="card-title grey-text text-darken-4"
-              ><a
-                :style="{ color: 'black' }"
-                :href="book.volumeInfo.infoLink"
-                target="_blank"
-                ><i class="material-icons right">info_outline</i></a
-              ></span
-            >
-          </div>
-          <div class="card-reveal">
-            <span
-              v-if="book.volumeInfo.title"
-              class="card-title grey-text text-darken-4"
-              >{{ book.volumeInfo.title
-              }}<i class="material-icons right">close</i></span
-            >
-            <p v-if="book.volumeInfo.description">
-              {{ book.volumeInfo.description }}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div> -->
+  
 
       <!-- search title-->
       <div class="page-title page-title--thin page-title--search">
