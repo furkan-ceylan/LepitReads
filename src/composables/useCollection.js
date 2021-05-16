a51fe6066a3e1bd1b8af4bc7f166bc0f8ac79d84
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-16 06:51:01 +0300

    composables pending update

--- src/composables/useCollection.js
+++ src/composables/useCollection.js
@@ -4,21 +4,25 @@ import { projectFirestore } from '../firebase/config'
 const useCollection = (collection) => {
 
   const error = ref(null)
+  const isPending = ref(false)
 
   // add a new document
   const addDoc = async (doc) => {
     error.value = null
+    isPending.value = true
 
     try {
       await projectFirestore.collection(collection).add(doc)
+      isPending.value = false
     }
     catch(err) {
       console.log(err.message)
       error.value = 'could not send the message'
+      isPending.value = false
     }
   }
 
-  return { error, addDoc }
+  return { error, addDoc, isPending }
 
 }
 
