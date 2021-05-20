b3169b9884a476818d3d7c5a5b6525d639262cc5
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-20 21:25:53 +0300

    firebase config changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,53 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzPb-zzH6uAPWpacD1a_TOsOnsfLJ8t_U",
-    authDomain: "lepitreads.firebaseapp.com",
-    projectId: "lepitreads",
-    storageBucket: "lepitreads.appspot.com",
-    messagingSenderId: "807283930460",
-    appId: "1:807283930460:web:f82c9cea8e30343c8cfd99",
-    measurementId: "G-LLLNXGKXWS"
-  };
+  apiKey: "AIzaSyAzPb-zzH6uAPWpacD1a_TOsOnsfLJ8t_U",
+  authDomain: "lepitreads.firebaseapp.com",
+  projectId: "lepitreads",
+  storageBucket: "lepitreads.appspot.com",
+  messagingSenderId: "807283930460",
+  appId: "1:807283930460:web:f82c9cea8e30343c8cfd99",
+  measurementId: "G-LLLNXGKXWS",
+};
 
-  firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-  const projectFirestore = firebase.firestore()
-  const projectAuth = firebase.auth()
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-  export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, timestamp };
+
+//create doc
+
+// const db = firebase.firestore();
+// const booksCollection = db.collection("books");
+
+// export const createBookList = (book) => {
+//   return booksCollection.add(book);
+// };
+
+// export const getBook = async (id) => {
+//   const book = await booksCollection.doc(id).get();
+//   return book.exists ? book.data() : null;
+// };
+
+// export const updateBook = (id, book) => {
+//   return booksCollection.doc(id).update(book);
+// };
+
+// export const deleteBook = (id) => {
+//   return booksCollection.doc(id).delete();
+// };
+
+// export const useLoadBook = () => {
+//   const books = ref([]);
+//   const close = booksCollection.onSnapshot((snapshot) => {
+//     books.value = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+//   });
+//   onUnmounted(close);
+//   return books;
+// };
